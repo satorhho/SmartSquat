8dabb0c926965d87bfa4eebd433119cc9f426fb9
satorhho <lbriones@gbox.adnu.edu.ph>
2021-12-09 12:42:12 +0800

    Initial commit + camera access feature

--- lib/Start.dart
+++ lib/Start.dart
@@ -1,10 +1,40 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:io';
 
-class Start extends StatelessWidget {
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class Start extends StatefulWidget {
+  @override
+  _StartState createState() => _StartState();
+
+}
+
+class _StartState extends State<Start> {
+  File? _image;
+  final image_picker = ImagePicker();
+
+  Future getImage() async {
+    final image = await image_picker.getImage(
+        source: ImageSource.camera
+    );
+
+    setState(() {
+      _image = File(image!.path);
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      body: Center(
+        child: _image == null ? Text("No Image Selected") : Image.file(_image!),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blue,
+        child: Icon(Icons.camera_alt),
+        onPressed: getImage,
+      ),
+    );
   }
-  
+
 }
